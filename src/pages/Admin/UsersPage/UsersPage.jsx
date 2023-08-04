aa41ccb4fbdfb6f912ac1b3edd1f7dc88fa93bfa
Farasat Khan <farasatkahan@gmail.com>
2023-08-04 21:43:56 +0500

    fixed table issues in UserPage

--- src/pages/Admin/UsersPage/UsersPage.jsx
+++ src/pages/Admin/UsersPage/UsersPage.jsx
@@ -88,7 +88,7 @@ const UsersPage = () => {
             <div className="border border-slate-100 m-3 rounded-lg">
               <div className="flex justify-between m-3">
                 <div className="mt-3">
-                  <div className="hidden sm:flex bg-slate-100 rounded-md">
+                  <div className="hidden md:flex bg-slate-100 rounded-md">
                     <div className="flex justify-center items-center p-3">
                       <BiSearch size={20} />
                     </div>
@@ -97,23 +97,24 @@ const UsersPage = () => {
                       placeholder="Search User"
                       value={searchQuery}
                       onChange={handleSearchQuery}
-                      className="w-30 sm:w-60 md:w-80 p-2 bg-transparent focus:outline-none"
+                      // w-30 sm:w-60 md:w-80
+                      className="p-2 bg-transparent focus:outline-none"
                     />
                   </div>
-                  <div className="sm:hidden p-3">
+                  <div className="md:hidden p-3">
                     <BiSearch size={25} />
                   </div>
                 </div>
-                <div className="flex">
+                <div className="flex justify-end">
                   {/* export */}
-                  <div className="flex justify-center items-center gap-3 border border-slate-100 rounded-lg m-3 w-32 h-10">
+                  <div className="px-3 flex justify-center items-center gap-3 border border-slate-100 rounded-lg m-3 w-32 h-10">
                     <div className="flex justify-center items-center">
                       <BsDownload size={20} />
                     </div>
                     <p>Export</p>
                   </div>
                   {/* filter */}
-                  <div className="flex justify-center items-center gap-3 border border-slate-100 rounded-lg m-3 w-32 h-10">
+                  <div className="px-3 flex justify-center items-center gap-3 border border-slate-100 rounded-lg m-3 w-32 h-10">
                     <div className="flex justify-center items-center">
                       <BsFilter size={20} />
                     </div>
