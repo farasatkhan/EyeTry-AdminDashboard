38208d6bc61b0ad3fac93cb3dcb118766545fb40
Farasat Khan <farasatkahan@gmail.com>
2023-08-17 15:09:27 +0500

    added latest transactions table.

--- src/pages/Admin/Home/Home.jsx
+++ src/pages/Admin/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../../../layouts/Admin/Card";
 
@@ -6,7 +6,18 @@ import Sales from "../../../components/ui/Admin/Graphs/Sales/Sales";
 import Traffic from "../../../components/ui/Admin/Graphs/Traffic";
 import OrdersTable from "../../../layouts/Admin/Orders/OrdersTable";
 
+import data from "../../../data/Admin/viewAllOrdersData";
+
+import { BiSearch } from "react-icons/bi";
+import { BsDownload, BsFilter } from "react-icons/bs";
+
 const Home = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearchQuery = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
   return (
     <div className="flex flex-col my-10">
       <div className="flex flex-grow justify-between mx-7 mt-7">
@@ -59,7 +70,58 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-col lg:flex-row gap-5 mx-7 mt-5"></div>
+      <div className="flex flex-col lg:flex-row gap-5 mx-7 mt-5">
+        <div className="w-full lg:w-12/12 border shadow-sm rounded-lg mt-5 lg:mt-0">
+          <div className="px-5 py-5">
+            <h1 className="text-xl font-bold">Latest Transactions</h1>
+          </div>
+          <div className="w-full h-0.5 bg-slate-100"></div>
+          <div className="">
+            <div className="">
+              <div className="flex justify-between m-3">
+                <div className="mt-3">
+                  <div className="hidden md:flex bg-slate-100 rounded-md">
+                    <div className="flex justify-center items-center p-3">
+                      <BiSearch size={20} />
+                    </div>
+                    <input
+                      type="text"
+                      placeholder="Search Orders"
+                      value={searchQuery}
+                      onChange={handleSearchQuery}
+                      // w-30 sm:w-60 md:w-80
+                      className="p-2 bg-transparent focus:outline-none"
+                    />
+                  </div>
+                  <div className="md:hidden p-3">
+                    <BiSearch size={25} />
+                  </div>
+                </div>
+                <div className="flex justify-end">
+                  {/* export */}
+                  <div className="px-3 flex justify-center items-center gap-3 border border-slate-100 rounded-lg m-3 w-32 h-10">
+                    <div className="flex justify-center items-center">
+                      <BsDownload size={20} />
+                    </div>
+                    <p>Export</p>
+                  </div>
+                  {/* filter */}
+                  <div className="px-3 flex justify-center items-center gap-3 border border-slate-100 rounded-lg m-3 w-32 h-10">
+                    <div className="flex justify-center items-center">
+                      <BsFilter size={20} />
+                    </div>
+                    <p>Filter</p>
+                  </div>
+                </div>
+              </div>
+              {/* table */}
+              <div className="mx-4">
+                <OrdersTable data={data} query={searchQuery} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
