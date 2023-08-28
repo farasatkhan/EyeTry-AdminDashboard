25871999759b60eb7376daebb2b9b4f1cf042de6
Farasat Khan <farasatkahan@gmail.com>
2023-08-28 19:51:00 +0500

    created ui for giftcards.

--- src/pages/Admin/Giftcards/IssueGiftcard/IssueGiftcard.jsx
+++ src/pages/Admin/Giftcards/IssueGiftcard/IssueGiftcard.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const IssueGiftcard = () => {
+  const [expirationDate, setExpirationDate] = useState(new Date());
+
   return (
     <div className="mx-5 md:mx-10 lg:mx-28 my-10">
       <div className="mb-10">
@@ -12,7 +17,7 @@ const IssueGiftcard = () => {
             <div className="mb-5">
               <span>Gift card details</span>
             </div>
-            <div className="flex flex-col gap-1 my-2">
+            <div className="flex flex-col gap-1 mb-5">
               <label htmlFor="giftcard_code" className="text-sm">
                 Gift card code
               </label>
@@ -22,16 +27,63 @@ const IssueGiftcard = () => {
                 className="px-2 py-1.5 border shadow-sm rounded-md outline-none text-sm"
               />
             </div>
-            <div className="flex flex-col gap-1 my-2">
+            <div className="flex flex-col gap-1 mb-5">
               <label htmlFor="value" className="text-sm">
                 Value
               </label>
-              <input
-                id="value"
-                type="text"
-                className="px-2 py-1.5 border shadow-sm rounded-md outline-none text-sm"
-                placeholder="0.00"
-              />
+              <div className="flex flex-row">
+                <input
+                  id="value"
+                  type="text"
+                  className="w-10/12 px-2 py-1.5 border shadow-sm rounded-s-md outline-none text-sm"
+                  placeholder="0.00"
+                />
+                <select
+                  id="currency"
+                  className="w-2/12 border px-1 sm:px-3 py-1 rounded-e-md outline-none text-sm cursor-pointer"
+                >
+                  <option key="USD" value="USD" className="cursor-pointer">
+                    USD
+                  </option>
+                  <option key="PKR" value="PKR" className="cursor-pointer">
+                    PKR
+                  </option>
+                </select>
+              </div>
+            </div>
+            <div className="flex flex-row gap-20 mb-5">
+              <div className="flex flex-col">
+                <label htmlFor="giftcard_status" className="text-sm mb-2">
+                  Status
+                </label>
+                <select
+                  id="giftcard_status"
+                  className="border px-5 py-2 rounded-md outline-none text-sm cursor-pointer"
+                >
+                  <option
+                    key="active"
+                    value="active"
+                    className="cursor-pointer"
+                  >
+                    Active
+                  </option>
+                  <option
+                    key="expired"
+                    value="expired"
+                    className="cursor-pointer"
+                  >
+                    Expired
+                  </option>
+                </select>
+              </div>
+              <div className="flex flex-col">
+                <span className="mb-2 text-sm">Expiration Date</span>
+                <DatePicker
+                  selected={expirationDate}
+                  onChange={(date) => setExpirationDate(date)}
+                  className="border rounded-md px-2 py-1.5 text-gray-800 w-full outline-none cursor-pointer"
+                />
+              </div>
             </div>
           </div>
         </div>
